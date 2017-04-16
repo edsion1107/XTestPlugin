0dca7d2a8f44083a26d0cfababed6792723c6762
edsion <edsion@i1hao.com>
2017-04-16 17:13:05 +0800

    修改同步路径，配置working directory，防止过多同步无用文件

--- XTestPlugin/debug.sh
+++ XTestPlugin/debug.sh
@@ -2,7 +2,7 @@
 # pycharm下"一键调试"脚本
 
 # 同步文件变更
-rsync -a . ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+rsync -a . ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/XTestPlugin/
 
 # 启动sublime（需命令行支持）
 subl ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/XTestPlugin/main.py
