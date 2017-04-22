0f6ac001a95847978aa9ddedaed6c518a21f8fa0
edsion <edsion@i1hao.com>
2017-04-22 19:58:52 +0800

    增加log装饰器 初步实现setup_adb 实现adb的devices、push、shell命令 实现通过pm命令判断apk是否安装 清理adb输出的方法clean_adb_output 实现start_xtest及对应配置文件的更新
    添加macOS下的adb v1.0.39

--- XTestPlugin/main.py
+++ XTestPlugin/main.py
@@ -3,6 +3,9 @@ import os
 import re
 import sublime
 import sublime_plugin
+import subprocess
+import traceback
+import threading
 from .errors import *
 
 SETTINGS = sublime.load_settings('XTestPlugin.sublime-settings')
@@ -11,10 +14,116 @@ ADB = SETTINGS.get('adb')
 
 # TODO:add logging file handler
 
-def setup_adb(adb_path):
-    sublime.platform()
-    # TODO：检查adb可用性
-    pass
+def log(func):
+    def wrapper(*args, **kwargs):
+        print("name:{0}, args:{1}, kwargs:{2}".format(func.__name__, args, kwargs))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def setup_adb():
+    global ADB
+    ADB = SETTINGS.get('adb')
+    if os.path.isfile(ADB) and os.path.exists(ADB):
+        return ADB
+    else:
+        platform = sublime.platform()
+        ADB = os.path.join(os.path.dirname(__file__), platform, 'adb')
+        return ADB
+        # TODO : 根据系统，使用自带adb。完成后，将设置中adb置为None
+
+
+def clean_adb_output(output_string):
+    output = str(output_string)
+    replace = [
+        'WARNING: linker: app_process has text relocations. This is wasting memory and is a security risk. Please fix.',
+    ]
+    for i in replace:
+        output = re.sub(i, '', output)
+    return output
+
+
+@log
+def adb_command(command, sn=None, timeout=5):  # timeout设置较短的默认值，是为了保证尽快给用户一个反馈，在使用具体命令时可以酌情处理
+    global ADB
+    setup_adb()
+    if sn:
+        sn = " -s {0}".format(sn)
+    else:
+        sn = ''
+    cmd = '''"{0}" {1} {2}'''.format(ADB, sn, command)
+    print('cmd:\n{0},\ntimeout:{1}'.format(cmd, timeout))
+    # 注意adb路径可能存在空格
+    p = subprocess.Popen(args=cmd, stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE, shell=True, bufsize=1, env={'ANDROID_HOME': os.path.dirname(ADB)})
+    return_code = None
+    try:
+        return_code = p.wait(timeout)
+    except subprocess.TimeoutExpired:
+        p.terminate()
+        print(traceback.print_exc())
+        return_code = 1
+    finally:
+        print('return code: {0}, pid:{1}'.format(return_code, p.pid))
+        stdout = p.stdout.read().decode()
+        stderr = p.stderr.read().decode()
+        try:  # 防止僵尸进程
+            p.kill()
+        except ProcessLookupError:
+            pass
+        return return_code, clean_adb_output(stdout), clean_adb_output(stderr)
+
+
+def adb_devices():
+    adb_command('start-server')
+    _, stdout, stderr = adb_command('devices')
+    devices = {}
+    if 'List of devices attached' in stdout:
+        devices_str = stdout
+    else:
+        devices_str = stderr
+    for line in devices_str.split('\n'):
+        if '\t' in line:
+            k, v = line.split('\t')
+            devices[k] = v
+
+    if len(devices) == 0:
+        sublime.error_message(stdout)
+        return None
+    else:
+        return devices
+
+
+def adb_shell(command, timeout=3600):
+    return adb_command('shell "{0}"'.format(command), timeout=timeout)
+
+
+def adb_shell_package_is_install(package_name):
+    _, stdout, stderr = adb_shell('pm list packages')
+    packages = [i.replace('package:', '') for i in stdout.split('\r\n') if len(i) > 0]
+    if package_name in packages:
+        return True
+    else:
+        print(packages)
+        sublime.error_message('"{0}" not installed!'.format(package_name))
+        return False
+
+
+def adb_push(local_file, remote_file=None):
+    # TODO:timeout按照（实测000）2M/s的速度计算
+    assert os.path.exists(local_file)
+    speed = 2 * 1024 * 1024  # usb传输速度，默认按照usb2.0计算，实测2M/s【另外可参考：https://www.zhihu.com/question/20186057】
+    time_out = int(os.stat(local_file).st_size / speed) + 1
+    if not remote_file:
+        remote_file = SETTINGS.get('xtest_remote')
+    return_code, stdout, stderr = adb_command('push "{0}" "{1}"'.format(local_file, remote_file), timeout=time_out)
+    if return_code == 0:
+        return True
+    elif return_code == 1:
+        return False
+    else:
+        return return_code
 
 
 def multiple_adb():
@@ -34,9 +143,9 @@ def list_dir(dir_path):
     return res
 
 
-def push_files(adb, file_list):
-    white_list = sublime.load_settings('XTestPlugin.sublime-settings').get("files_white_list")
-    black_list = sublime.load_settings('XTestPlugin.sublime-settings').get("files_black_list")
+def push_files(file_list, remote_path=None):
+    white_list = SETTINGS.get("files_white_list")
+    black_list = SETTINGS.get("files_black_list")
 
     def filter_by_re(text, pattern_list):
         for pattern in pattern_list:
@@ -49,9 +158,22 @@ def push_files(adb, file_list):
     white = filter(lambda x: filter_by_re(x, white_list), file_list)
     black = filter(lambda x: filter_by_re(x, black_list), file_list)
     files = set(file_list) - set(black) | set(white)
-    # for i in files:
-    #     print(i)
-    # TODO:封装adb命令
+    success = list(filter(lambda x: adb_push(x, remote_path) is True, files))
+    if len(success) != len(files):
+        print('Warning:{0} of {1} pushed'.format(len(success), len(files)))
+
+
+def start_xtest():
+    _, stdout, stderr = adb_shell(
+        'log -p i -t \"XTest XTest start!\" && am instrument -e class {0} -w {1}/{2}'.format(
+            SETTINGS.get("xtest_replay_class"), SETTINGS.get("xtest_package_name"),
+            SETTINGS.get("xtest_replay_activity"),
+            timeout=SETTINGS.get("xtest_timeout"))
+    )
+
+
+def stop_xtest():
+    pass
 
 
 class ExampleCommand(sublime_plugin.WindowCommand):
@@ -60,8 +182,13 @@ class ExampleCommand(sublime_plugin.WindowCommand):
         print(self.__class__.__name__)
         self.window.set_sidebar_visible(True)  # 设置打开sidebar
 
+        if adb_devices() is None:  # 检查是否有设备连接
+            return
+        if not adb_shell_package_is_install(SETTINGS.get('xtest_package_name')):  # 检查XTest是否安装
+            return
+
         # 确定文件（脚本）路径
-        files_root_path = None
+        files_root_path = os.getcwd()
         current_file_path = self.window.active_view().file_name()
         if current_file_path:
             files_root_path = os.path.dirname(current_file_path)
@@ -74,7 +201,13 @@ class ExampleCommand(sublime_plugin.WindowCommand):
                 files_root_path = self.window.folders()[0]  # 只有一个文件夹的情况
         # print(files_root_path)
         files = list_dir(files_root_path)
-        push_files(ADB, file_list=files)
+        push_files(file_list=files)
+
+        # TODO: 根据paramer.lua中的包名，判断apk是否安装
+        stop_xtest()
+        start_xtest()
+        # threading.Thread(target=start_xtest, name=start_xtest.__name__)
+        # TODO： showlog方案改进：解析Main.lua中最后一个脚本，然后找到最后结束的关键字。通过tail -f 来实时显示日志
 
 
 class DoctorCommand(sublime_plugin.TextCommand):
