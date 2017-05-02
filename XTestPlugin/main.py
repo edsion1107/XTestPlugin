b80295dad882de8eebf37158c1b1fdf4bae92240
edsion <edsion@i1hao.com>
2017-05-02 20:16:59 +0800

    增加多设备支持

--- XTestPlugin/main.py
+++ XTestPlugin/main.py
@@ -4,6 +4,8 @@ import re
 import time
 import subprocess
 import threading
+from multiprocessing.dummy import Pool
+from multiprocessing.sharedctypes import Array
 from subprocess import TimeoutExpired
 import sublime
 import sublime_plugin
@@ -11,13 +13,24 @@ from .errors import *
 
 SETTINGS = sublime.load_settings('XTestPlugin.sublime-settings')
 ADB = SETTINGS.get('adb')
+SN = ''
 
 
 # TODO:add logging file handler
 
-def log(func):
+def select_device(func):
     def wrapper(*args, **kwargs):
-        print("name:{0}, args:{1}, kwargs:{2}".format(func.__name__, args, kwargs))
+        global SN
+        if len(SN) > 0:
+            if SETTINGS.get('debug'):
+                print('device:[{0}] selected!'.format(SN))
+            if 'command' in kwargs:
+                kwargs['command'] = '-s {0} {1}'.format(SN, kwargs['command'])
+            else:
+                args = list(args)
+                args = tuple(['-s {0} {1}'.format(SN, args[0])] + args[1:])
+        if SETTINGS.get('debug'):
+            print("name:{0}, args:{1}, kwargs:{2}".format(func.__name__, args, kwargs))
         return func(*args, **kwargs)
 
     return wrapper
@@ -29,7 +42,7 @@ def init_adb():
     if os.path.isfile(ADB) and os.path.exists(ADB):
         print('adb exists!')
     else:
-        platform = sublime.platform()
+        platform = sublime.platform().lower()
         ADB = os.path.join(os.path.dirname(__file__), platform, 'adb')
     os.putenv('PATH', '{0}:{1}'.format(os.getenv('PATH'), os.path.dirname(ADB)))
     return ADB
@@ -45,17 +58,12 @@ def clean_adb_output(output_string):
     return output
 
 
-def adb_command(command, timeout=5):
-    cmd = 'adb ' + command
-    print("\tadb command:{0}\n\ttimeout:{1}".format(command, timeout))
-    return subprocess.check_output(args=cmd, stderr=subprocess.STDOUT, universal_newlines=True,
-                                   timeout=timeout, shell=True)
-
-
+@select_device
 def adb_cmd(command, time_out=10):
     p = subprocess.Popen('adb {0}'.format(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                          cwd=os.path.dirname(__file__), universal_newlines=False, bufsize=1, shell=True)
-    # print(p.args)
+    if SETTINGS.get('debug'):
+        print(p.args)
     try:
         # print('>pid:{0}, return code:{1}'.format(p.pid, p.returncode))
         res = p.communicate(timeout=time_out)[0]
@@ -65,13 +73,9 @@ def adb_cmd(command, time_out=10):
     else:
         if res:
             return res.decode()
-            # finally:
-            #     print('>pid:{pid}, return_code:{return_code}'.format(pid=p.pid, return_code=p.returncode))
 
 
 def adb_devices():
-    # adb_command('start-server')
-    # stdout = adb_command('devices')
     res = adb_cmd('devices', time_out=10)
     devices = {}
     for line in res.split('\n'):
@@ -89,13 +93,17 @@ def adb_devices():
         return devices
 
 
-def adb_shell_package_is_install(package_name):
-    # stdout = adb_command('shell pm list packages')
+def adb_shell_pm_list_packages():
     res = adb_cmd('shell "pm list packages"', time_out=5)
     # 按照换行符'\n'切割为列表，再使用strip()去掉可能存在的'\r'。可以避免不同平台换行不一致的问题
+    return [i.strip().replace('package:', '') for i in res.split('\n')]
+
+
+def package_is_install(package_name, packages=None):
+    if not packages:
+        packages = adb_shell_pm_list_packages()
     result = False
-    for line in res.split('\n'):
-        line = line.strip().replace('package:', '')
+    for line in packages:
         if package_name in line:
             result = True
             break
@@ -103,7 +111,7 @@ def adb_shell_package_is_install(package_name):
             continue
     else:
         sublime.error_message('"{0}" not installed!'.format(package_name))
-        print(res)
+        print(packages)
     return result
 
 
@@ -122,19 +130,12 @@ def adb_shell_ps_process_is_running(proc):
 
 
 def adb_push(local_file, remote_file=None):
-    # TODO:timeout按照（实测000）2M/s的速度计算
     assert os.path.exists(local_file)
-    speed = 2 * 1024 * 1024  # usb传输速度，默认按照usb2.0计算，实测2M/s【另外可参考：https://www.zhihu.com/question/20186057】
+    speed = 2 * 1024 * 1024  # usb传输速度，按照2M/s速度进行估算
     time_out = int(os.stat(local_file).st_size / speed) + 3
     if not remote_file:
         remote_file = '/sdcard/'
-    # return adb_command('push "{0}" "{1}"'.format(local_file, remote_file), timeout=time_out)
     return adb_cmd('push "{0}" "{1}"'.format(local_file, remote_file), time_out=time_out)
-
-
-def multiple_adb():
-    # TODO：多个设备时需要选择
-    pass
 
 
 def list_dir(dir_path):
@@ -164,10 +165,14 @@ def push_files(file_list, remote_path=None):
     white = filter(lambda x: filter_by_re(x, white_list), file_list)
     black = filter(lambda x: filter_by_re(x, black_list), file_list)
     files = set(file_list) - set(black) | set(white)
-
-    # TODO：可以改为进程池，看能否提高性能
+    #  使用线程池，提高效率
+    # pool = Pool()
+    # pool.map_async(lambda i: adb_push(local_file=i, remote_file=remote_path), files)
+    # pool.close()
+    # pool.join()
+    # print('pool done')
     for i in files:
-        adb_push(i, remote_path)
+        adb_push(local_file=i, remote_file=remote_path)
 
 
 def show_log_and_error(params):
@@ -211,32 +216,62 @@ def show_log_and_error(params):
 
 
 def force_stop():
-    # adb_command('shell service call activity 79 s16 {0}'.format(SETTINGS.get('xtest').get('package_name')))
     adb_cmd('shell service call activity 79 s16 {0}'.format(SETTINGS.get('xtest').get('package_name')), time_out=10)
-    # adb_command('shell service call activity 79 s16 {0}'.format(SETTINGS.get('kat').get('package_name')))
     adb_cmd('shell service call activity 79 s16 {0}'.format(SETTINGS.get('kat').get('package_name')), time_out=10)
 
 
-class ExampleCommand(sublime_plugin.WindowCommand):
-    # TODO：如果执行时，缓冲区更改未保存，则给出提示
+class AutotestCommand(sublime_plugin.WindowCommand):
+    devices = {}
+    online_devices = []
+    selected_device = ''
+
+    def select_device(self, devices):
+        self.devices = devices
+        self.online_devices = [i for i in devices if 'device' in devices[i]]
+        if len(devices) == 0:
+            return
+        elif len(devices) > 1:
+            self.window.show_quick_panel(items=self.online_devices, on_select=self.device_selected,
+                                         flags=sublime.KEEP_OPEN_ON_FOCUS_LOST)
+        else:
+            self.selected_device = list(devices)[0]
+            global SN
+            SN = self.selected_device
+            self.load_settings()
+
+    def device_selected(self, *args):
+        index = args[0]
+        self.selected_device = self.online_devices[index]
+        global SN
+        SN = self.selected_device
+        self.load_settings()
+
     def run(self):
         print(self.__class__.__name__)
-        self.window.set_sidebar_visible(True)  # 设置打开sidebar
+        self.window.set_sidebar_visible(True)  # 设置打开side bar
+        self.window.set_status_bar_visible(True)  # 设置打开status bar
+        self.window.status_message('[{0}]preparing...'.format(time.time()))
         init_adb()
         adb_cmd(command='kill-server', time_out=10)  # 【debug】清理进程，防止干扰本次测试。之后可能需要注释掉防止影响多设备运行
-        if adb_devices() is None:  # 检查是否有设备连接
-            return
+        devices = adb_devices()
+        self.select_device(devices)
+
+    def load_settings(self):
+        # 读取配置
         if SETTINGS.get("is_xtest"):
-            params = SETTINGS.get('xtest')
-            command = 'shell "log i \'start\' && am instrument -e class {class_name} -w {package_name}/{activity}"'
+            self.params = SETTINGS.get('xtest')
+            self.run_test_command = 'shell "log i \'start\' && ' \
+                                    'am instrument -e class {class_name} -w {package_name}/{activity} &&' \
+                                    ' log i \'end\'"'
         else:
-            params = SETTINGS.get('kat')
-            command = 'shell "log i \'start\' && am instrument -w {package_name}/{activity}"'
+            self.params = SETTINGS.get('kat')
+            self.run_test_command = 'shell "log i \'start\' ' \
+                                    '&& am instrument -w {package_name}/{activity} &&' \
+                                    ' log i \'end\' "'
+        self.find_and_push_files()
 
-        if not adb_shell_package_is_install(params.get('package_name')):  # 检查XTest是否安装
-            return
-
-        # 确定文件（脚本）路径
+    def find_and_push_files(self):
+        # 确定文件（脚本）路径，并push到设备
         files_root_path = os.getcwd()
         current_file_path = self.window.active_view().file_name()
         if current_file_path:
@@ -248,20 +283,45 @@ class ExampleCommand(sublime_plugin.WindowCommand):
                 sublime.error_message(no_file_or_folder)
             else:
                 files_root_path = self.window.folders()[0]  # 只有一个文件夹的情况
-        # print(files_root_path)
         files = list_dir(files_root_path)
-        push_files(file_list=files, remote_path=params.get('remote_path'))
+        push_files(file_list=files, remote_path=self.params.get('remote_path'))
+        self.check_packages()
 
-        # TODO: 根据paramter.lua中的包名，判断apk是否安装
+    def check_packages(self):
+        installed_packages = adb_shell_pm_list_packages()  # 获取设备中已安装app的列表
+        if not package_is_install(self.params.get('package_name'), installed_packages):  # 检查XTest/KAT是否安装
+            return
+
+        # 根据Parameter.lua中的包名，判断apk是否安装
+        parameter_lua_path = self.params.get('remote_path')
+        if parameter_lua_path.endswith('/'):
+            parameter_lua = adb_cmd('shell cat {0}Parameter.lua'.format(parameter_lua_path))
+        else:
+            parameter_lua = adb_cmd('shell cat {0}/Parameter.lua'.format(parameter_lua_path))
+        app_package_name = None
+        for line in parameter_lua.split('\n'):
+            res = re.search('^\s*PackageName\s*=\s*[\'"](\S+)[\'"].*', line, re.IGNORECASE)
+            if res:
+                app_package_name = res.groups()[0]
+                break
+        if not package_is_install(app_package_name, installed_packages):
+            return
+        self.start_test()
+
+    def start_test(self):
         force_stop()  # 可以同时杀掉XTest/kat的进程（仅启动app但未运行自动化测试也会被kill），这样后面才能根据ps结果判断是否在运行
 
-        command = command.format(class_name=params.get("class"), package_name=params.get("package_name"),
-                                 activity=params.get("activity"), )
-
-        run_test = threading.Thread(target=adb_cmd, kwargs={'command': command, 'time_out': params.get('time_out')},
+        # 启动脚本
+        run_test_command = self.run_test_command.format(class_name=self.params.get("class"),
+                                                        package_name=self.params.get("package_name"),
+                                                        activity=self.params.get("activity"), )
+        run_test = threading.Thread(target=adb_cmd,
+                                    kwargs={'command': run_test_command, 'time_out': self.params.get('time_out')},
                                     daemon=True)
         run_test.start()
-        cat_log = threading.Thread(target=show_log_and_error, args=(params,), daemon=True)
+
+        # 实时显示log和error
+        cat_log = threading.Thread(target=show_log_and_error, args=(self.params,), daemon=True)
         cat_log.start()
 
 
